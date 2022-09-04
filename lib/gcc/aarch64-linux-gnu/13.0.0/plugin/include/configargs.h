eb2769eb40de37a54b40e54b9eb8c7559a60ade0
greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>
2022-09-04 14:01:46 +0700

    greenforce: Bump to 20220904 build
    GCC version: gcc version 13.0.0 20220904 (experimental) (GCC)
    Binutils version: GNU ld (GNU Binutils) 2.39.50.20220904
    GCC repo commit: Add real_isdenormal.
    Link: https://github.com/gcc-mirror/gcc/commit/6832dd39d
    
    Signed-off-by: greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>
    Change-Id: I149b91074895f57dd210d40b68b170fa2361fad0

---
+++ lib/gcc/aarch64-linux-gnu/13.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror CFLAGS='-g0 -O3 -pipe -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' CXXFLAGS='-g0 -O3 -pipe -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' LDFLAGS=-O3 --enable-languages=c,c++ --target=aarch64-linux-gnu --prefix=/tmp/cirrus-ci-build/aarch64-linux-gnu --with-linker-hash-style=both";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
