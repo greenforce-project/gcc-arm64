b473085545f94b89184963da3e483ce7d2e4a4d5
greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>
2022-09-10 09:17:38 +0700

    greenforce: Bump to 20220910 build
    GCC version: gcc version 13.0.0 20220910 (Gf Cross v13) (GCC)
    Binutils version: GNU ld (GNU Binutils) 2.39.50.20220910
    GCC repo commit: Daily bump.
    Link: https://github.com/gcc-mirror/gcc/commit/861d1a11c
    
    Change-Id: I1f0843efcf0f965057a1a201125aabbcac20834c

---
+++ lib/gcc/aarch64-elf/13.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot --with-headers=/usr/include";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
