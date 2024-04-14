2a35b7cfadbe67ac7896c5d2f31e5db6fe164e1b
greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>
2024-04-14 19:21:22 +0700

    greenforce: Bump to 14042024 build
    NOTE: The GCC prefix is changed here. (aarch64-elf -> aarch64-linux-gnu)
    
    GCC version: gcc version 14.0.1 20240414 (Gf Cross v14) (GCC)
    Binutils version: GNU ld (GNU Binutils) 2.42.50
    GCC repo commit: c++: Setup aliases imported from modules [PR106820]
    Link: https://github.com/gcc-mirror/gcc/commit/62a0ef0d0
    
    Change-Id: Ifa520360d841b3e18306498f28a466c81ff8e815
    Signed-off-by: greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>

--- lib/gcc/aarch64-elf/14.0.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++,fortran --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-sysroot";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
