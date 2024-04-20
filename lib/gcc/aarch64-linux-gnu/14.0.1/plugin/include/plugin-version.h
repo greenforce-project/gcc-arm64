596004caebb2a219322c584602b5721be5b48170
greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>
2024-04-20 08:06:11 +0700

    greenforce: Bump to 20042024 build
    GCC version: gcc version 14.0.1 20240420 (Gf Cross v14) (GCC)
    Binutils version: GNU ld (Gf Cross Binutils) 2.42.50
    GCC repo commit: Revert "RISC-V: Support one more overlap for wv instructions"
    Link: https://github.com/gcc-mirror/gcc/commit/0cbeafe26
    
    Change-Id: I212e09612dd218c6907582688de1acfc35d9623a
    Signed-off-by: greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>

--- lib/gcc/aarch64-linux-gnu/14.0.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-linux-gnu/14.0.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "14.0.1";
-static char datestamp[] = "20240415";
+static char datestamp[] = "20240420";
 static char devphase[] = "Gf Cross v14";
 static char revision[] = "";
 
